abfb07a580bdf5d3c906edae66e1b4e122ddfa87
Dennis <dennismoh88@gmail.com>
2026-04-13 20:59:11 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,12 @@
+export const characters = [
+  {name:"Spider-Man", universe:"Marvel", hp:100, attack:20},
+  {name:"Iron Man", universe:"Marvel", hp:120, attack:25},
+  {name:"Batman", universe:"DC", hp:110, attack:18},
+  {name:"Superman", universe:"DC", hp:150, attack:30}
+];
 
+export const locations = [
+  {name:"Queens", info:"Spider-Man lives here"},
+  {name:"Gotham", info:"Batman protects this city"},
+  {name:"Metropolis", info:"Superman's city"}
+];
